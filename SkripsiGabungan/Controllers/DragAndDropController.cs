c6dbb03edaab02ba986e38ae22f6688b8172c641
handytantyo <handy.tantyo14@student.surya.ac.id>
2018-05-15 11:33:06 +0700

    FirstUpload
    Version 1

---
+++ SkripsiGabungan/Controllers/DragAndDropController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bytescout.PDFExtractor;
+
+namespace SkripsiGabungan.Controllers
+{
+    public class DragAndDropController : Controller
+    {
+        // GET: DragAndDrop
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFiles(IEnumerable<HttpPostedFileBase> files)
+        {
+            //TextExtractor extractor = new TextExtractor();
+            //extractor.RegistrationName = "demo";
+            //extractor.RegistrationKey = "demo";
+
+            foreach (var file in files)
+            {
+                string filePath = "default" + Path.GetExtension(file.FileName);//Guid.NewGuid() + Path.GetExtension(file.FileName);
+                //string fileapa = Server.MapPath(filePath);
+                //extractor.LoadDocumentFromFile(fileapa);
+                //extractor.OCRMode = OCRMode.Auto;
+                //extractor.OCRLanguageDataFolder = @"C:\Users\Nabyl\Desktop\ocr1IronPDF\ocr1IronPDF\tessdata";
+                //extractor.OCRLanguage = "eng";
+                //extractor.OCRResolution = 300;
+                //extractor.SaveTextToFile(Path.Combine(Server.MapPath("~/UploadedFiles/output.txt"), filePath));
+                file.SaveAs(Path.Combine(Server.MapPath("~/UploadedFiles"), filePath));//filePath
+                //Here you can write code for save this information in your database if you want
+                Page_Load(Path.Combine(Server.MapPath("~/UploadedFiles"), filePath), filePath);
+
+            }
+
+            return Json("file uploaded successfully");
+        }
+
+        [NonAction]
+        protected void Page_Load(string filepath, string name)
+        {
+            String inputFile = filepath;
+            string namaFIle = name;
+
+            // Create Bytescout.PDFExtractor.TextExtractor instance
+            TextExtractor extractor = new TextExtractor();
+            extractor.RegistrationName = "demo";
+            extractor.RegistrationKey = "demo";
+
+            // Load sample PDF document
+            extractor.LoadDocumentFromFile(inputFile);
+
+            Response.Clear();
+            Response.ContentType = "text/html";
+
+            Response.Write("<pre>");
+
+            // Write extracted text to output stream
+            //extractor.SaveTextToStream(Response.OutputStream);
+
+            // Save extracted text to file
+            extractor.SaveTextToFile(Path.Combine(Server.MapPath("~/UploadedFiles"), namaFIle + "TextFIle"));
+            Response.Write("</pre>");
+
+            Response.End();
+        }
+
+        //public ActionResult Ocr()
+        //{
+        //    // Create Bytescout.PDFExtractor.TextExtractor instance
+        //    TextExtractor extractor = new TextExtractor();
+        //    extractor.RegistrationName = "demo";
+        //    extractor.RegistrationKey = "demo";
+
+        //    // Load sample PDF document
+        //    extractor.LoadDocumentFromFile("C:/Users/Nabyl/Desktop/ocr1IronPDF/ocr1IronPDF/bin/Debug/1ar-adhi2016.pdf");
+
+        //    // Enable Optical Character Recognition (OCR)
+        //    // in .Auto mode (SDK automatically checks if needs to use OCR or not)
+        //    extractor.OCRMode = OCRMode.Auto;
+
+        //    // Set the location of "tessdata" folder containing language data files
+        //    extractor.OCRLanguageDataFolder = @"C:\Users\Nabyl\Desktop\ocr1IronPDF\ocr1IronPDF\tessdata";
+
+        //    // Set OCR language
+        //    extractor.OCRLanguage = "eng"; // "eng" for english, "deu" for German, "fra" for French, "spa" for Spanish etc - according to files in /tessdata
+
+        //    // Set PDF document rendering resolution
+        //    extractor.OCRResolution = 300;
+
+        //    // Save extracted text to file
+        //    extractor.SaveTextToFile("C:/Users/Nabyl/Desktop/ocr1IronPDF/ocr1IronPDF/bin/Debug/adhi16.txt");
+
+        //    // Open output file in default associated application
+        //    System.Diagnostics.Process.Start("C:/Users/Nabyl/Desktop/ocr1IronPDF/ocr1IronPDF/bin/Debug/adhi16.txt");
+        //    return Json("Success..", JsonRequestBehavior.AllowGet);
+
+        //}
+    }
+}
---
+++ SkripsiGabungan/Controllers/hasilsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SkripsiGabungan.Models;
+
+namespace SkripsiGabungan.Controllers
+{
+    public class hasilsController : Controller
+    {
+        private tugasakhirEntities db = new tugasakhirEntities();
+
+        // GET: hasils
+        public ActionResult Index()
+        {
+            var hasils = db.hasils.Include(h => h.perusahaan).Include(h => h.sumber).Include(h => h.tahun);
+            return View(hasils.ToList());
+        }
+
+        public ActionResult GetData()
+        {
+            using (db)
+            {
+                db.Configuration.LazyLoadingEnabled = false;//most important
+                //List<hasil> empList = db.hasils.ToList<hasil>();
+                //var empList = db.hasils.OrderBy(a => a.id).ToList();
+                var join = (from hasil in db.hasils
+                            join perusahaan in db.perusahaans on hasil.id_perusahaan equals perusahaan.id
+                            join tahun in db.tahuns on hasil.id_tahun equals tahun.id
+                            select new { hasil.id, perusahaan.nama_perusahaan, tahun.tahun1, hasil.ROE, hasil.ROI, hasil.tingkat_kesehatan, hasil.grade });
+                //hasil.id digunakan untuk dipanggil function lain
+                var list = new List<hasil>();
+                foreach (var hasils in join)
+                {
+                    list.Add(new hasil()
+                    {
+                        id = hasils.id,
+                        nama_perusahaan = hasils.nama_perusahaan,
+                        tahun_1 = hasils.tahun1,
+                        grade = hasils.grade,
+                        tingkat_kesehatan = hasils.tingkat_kesehatan
+                    });
+                }
+                list.OrderBy(a => a.id).ToList();
+                return Json(new { data = list }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        // GET: hasils/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            hasil hasil = db.hasils.Find(id);
+            if (hasil == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hasil);
+        }
+
+        // GET: hasils/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_perusahaan = new SelectList(db.perusahaans, "id", "nama_perusahaan");
+            ViewBag.id = new SelectList(db.sumbers, "id", "id");
+            ViewBag.id_tahun = new SelectList(db.tahuns, "id", "id");
+            return View();
+        }
+
+        // POST: hasils/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,id_perusahaan,id_tahun,ROE,ROI,cash_ratio,current_ratio,CP,PP,TATO,TMS_TA,target,target_2,tingkat_kesehatan,grade")] hasil hasil)
+        {
+            if (ModelState.IsValid)
+            {
+                db.hasils.Add(hasil);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_perusahaan = new SelectList(db.perusahaans, "id", "nama_perusahaan", hasil.id_perusahaan);
+            ViewBag.id = new SelectList(db.sumbers, "id", "id", hasil.id);
+            ViewBag.id_tahun = new SelectList(db.tahuns, "id", "id", hasil.id_tahun);
+            return View(hasil);
+        }
+
+        // GET: hasils/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            hasil hasil = db.hasils.Find(id);
+            if (hasil == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_perusahaan = new SelectList(db.perusahaans, "id", "nama_perusahaan", hasil.id_perusahaan);
+            ViewBag.id = new SelectList(db.sumbers, "id", "id", hasil.id);
+            ViewBag.id_tahun = new SelectList(db.tahuns, "id", "id", hasil.id_tahun);
+            return View(hasil);
+        }
+
+        // POST: hasils/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,id_perusahaan,id_tahun,ROE,ROI,cash_ratio,current_ratio,CP,PP,TATO,TMS_TA,target,target_2,tingkat_kesehatan,grade")] hasil hasil)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hasil).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_perusahaan = new SelectList(db.perusahaans, "id", "nama_perusahaan", hasil.id_perusahaan);
+            ViewBag.id = new SelectList(db.sumbers, "id", "id", hasil.id);
+            ViewBag.id_tahun = new SelectList(db.tahuns, "id", "id", hasil.id_tahun);
+            return View(hasil);
+        }
+
+        // GET: hasils/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            hasil hasil = db.hasils.Find(id);
+            if (hasil == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hasil);
+        }
+
+        // POST: hasils/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            hasil hasil = db.hasils.Find(id);
+            db.hasils.Remove(hasil);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
